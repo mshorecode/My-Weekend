f0d65a4a0033ed6c08d7e5a92240d8aed2ad783e
Ryan Shore <mshorecode@gmail.com>
2023-11-23 11:50:00 -0600

    added logo file, changed background color, added logo to NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
@@ -5,24 +6,35 @@ import {
   Navbar, Container, Nav, Button,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
+import Logo from '../styles/images/logo.png';
+
+console.warn(Logo);
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" style={{ background: '#05014a' }}>
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>My Weekend</Navbar.Brand>
+          <Navbar.Brand style={{ color: '#fff', fontWeight: '600' }}>
+            My Weekend
+            {/* <img
+              src={Logo}
+              width="100"
+              height="50"
+              alt="Brand logo"
+            /> */}
+          </Navbar.Brand>
         </Link>
         <Nav className="me-auto">
           {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
           <Link passHref href="/">
-            <Nav.Link>Dashboard</Nav.Link>
+            <Nav.Link style={{ color: '#fff', fontWeight: '600' }}>Dashboard</Nav.Link>
           </Link>
           <Link passHref href="/">
-            <Nav.Link>Schedule</Nav.Link>
+            <Nav.Link style={{ color: '#fff', fontWeight: '600' }}>Schedule</Nav.Link>
           </Link>
           <Link passHref href="/">
-            <Nav.Link>Payments</Nav.Link>
+            <Nav.Link style={{ color: '#fff', fontWeight: '600' }}>Payments</Nav.Link>
           </Link>
         </Nav>
         <Button variant="danger" onClick={signOut}>Sign Out</Button>
