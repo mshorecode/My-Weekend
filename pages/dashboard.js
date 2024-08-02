eed8052d11394789005819e2a1bcea4cdfef1c97
Ryan Shore <mshorecode@gmail.com>
2024-08-02 00:35:56 -0500

    Testing 'Commit & Sync' functionality in VSCode

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -31,7 +31,7 @@ export default function Dashboard() {
     renderPayments();
   }, []);
 
-  if (household.length === 0) {
+  if (household.length === 1) {
     return (
       <div
         className="text-center d-flex flex-column justify-content-center align-content-center"
